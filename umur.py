8d1468864468085bf944cc357937f08fa7c72ed6
arunnikka <160417720+arunnikka@users.noreply.github.com>
2024-02-19 00:00:04 +0700

    Tugas Dasar Pemrograman

---
+++ umur.py
@@ -0,0 +1,6 @@
+TahunSekarang = float(input("Masukan Tahun Saat ini : "))
+TahunLahir = float(input("Tahun Lahirmu : "))
+
+Umur = TahunSekarang - TahunLahir
+
+print("Umur anda saat ini : %.0f" % Umur)
